34dcd5ebd30f8cd442241dd3fe65bc70f6619e49
anamariiab <119733993+anamariiab@users.noreply.github.com>
2024-03-14 13:40:25 +0100

    Creacion Articulo Y Compra App

---
+++ Grupo2AAD/src/Articulo.java
@@ -0,0 +1,10 @@
+
+public class Articulo {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
---
+++ Grupo2AAD/src/CompraApp.java
@@ -0,0 +1,8 @@
+
+public class CompraApp {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
