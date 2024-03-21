fa599238cb799d3180750fcf059209e6cf408b43
anamariiab <119733993+anamariiab@users.noreply.github.com>
2024-03-21 11:19:08 +0100

    main de nuevo

--- Grupo2AAD/src/StockArticulosMain.java
+++ Grupo2AAD/src/StockArticulosMain.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 public class StockArticulosMain {
-
+	
 	public static void main(String[] args) {
 
 		// Lista para almacenar los productos
